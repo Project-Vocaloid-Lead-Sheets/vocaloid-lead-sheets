23006866522632c6fc3dbb2171181eb065f4f36a
Matthew Zhong <mzhong99@vt.edu>
2026-07-24 18:44:30 -0400

    Basic discord sync command

--- discord-bot/src/discord_bot/bot_core.py
+++ discord-bot/src/discord_bot/bot_core.py
@@ -1,6 +1,9 @@
+import os
 import logging
 import discord
 import pathlib
+
+from discord_bot.github_client import GitHubClient
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 _logger = logging.getLogger(__name__)
@@ -34,6 +37,9 @@ class PvlsBotCore:
         self._register_events()
         self._register_commands()
 
+        # Since the GitHub client relies on aiohttp, you have to wait for the async loop to start to initialize one
+        self._github: GitHubClient = None
+
         self._sekai_counter = 0
 
     def _register_events(self):
@@ -42,6 +48,8 @@ class PvlsBotCore:
             _logger.info(f"Logged in as {self._client.user}")
             await self._tree.sync()
             _logger.info("Slash commands synced")
+            self._github = GitHubClient("Project-Vocaloid-Lead-Sheets", "vocaloid-lead-sheets", os.environ["GITHUB_TOKEN"])
+            _logger.info("GitHub client initialized!")
 
         @self._tree.error
         async def on_error(interaction, error):
@@ -60,6 +68,14 @@ class PvlsBotCore:
             line = SEKAI_TEXT[self._sekai_counter % len(SEKAI_TEXT)]
             self._sekai_counter += 1
             await interaction.response.send_message(line)
+
+        @group.command(name="sync", description="Sync and deploy website with updated Google Drive contents")
+        async def sync(interaction: discord.Interaction):
+            calling_user = interaction.user
+            _logger.info(f"User {calling_user.display_name} ({calling_user.id}) started a site sync and deploy")
+            await self._github.dispatch_workflow("content-sync-and-deploy.yml")
+            await interaction.response.send_message("Content sync started!")
+
 
         self._tree.add_command(group)
 
---
+++ discord-bot/src/discord_bot/github_client.py
@@ -0,0 +1,32 @@
+import aiohttp
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+_logger = logging.getLogger(__name__)
+
+class GitHubClient:
+    def __init__(self, owner: str, repo: str, token: str):
+        self._owner = owner
+        self._repo = repo
+
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "authorization": f"Bearer {token}",
+            "X-GitHub-Api-Version": "2022-11-28",
+            "User-Agent": "pvls-automation/1.0",
+        }
+
+        self._session = aiohttp.ClientSession(headers=headers)
+
+    async def close(self) -> None:
+        await self._session.close()
+
+    async def dispatch_workflow(self, workflow: str, *, ref: str = "main", inputs: dict[str, any] | None = None):
+        url = f"https://api.github.com/repos/{self._owner}/{self._repo}/actions/workflows/{workflow}/dispatches"
+        payload = {"ref": ref, "inputs": inputs or {}}
+
+        async with self._session.post(url, json=payload) as response:
+            body = await response.text()
+            _logger.info("Found: {response.body}")
+            if response.status != 204:
+                raise RuntimeError(f"GitHub workflow dispatch failed ({response.status}): {body}")
