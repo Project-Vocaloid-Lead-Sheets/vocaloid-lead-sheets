70fbfce966b0064f63e32e8a2421cc6bf51fb68d
Matthew Zhong <mzhong99@vt.edu>
2026-08-16 12:08:54 -0400

    Add command to discord bot to autofill options for songs to sync

--- discord-bot/src/discord_bot/bot_core.py
+++ discord-bot/src/discord_bot/bot_core.py
@@ -16,6 +16,10 @@ SCRIPT_DIR = pathlib.Path(__file__).parent
 SEKAI_TEXT = (SCRIPT_DIR / "sekai.txt").read_text(encoding="utf-8").splitlines()
 BOT_CHANNEL_NAME = "bot-terminal"
 
+# For some crazy reason, Discord caps the autofill count to 25 and will just reject the command if you try to supply
+# anything more than that
+DISCORD_AUTOFILL_COUNT_MAX = 25
+
 
 def asyncio_exception_handler(loop: asyncio.AbstractEventLoop, context: dict[str, any]) -> None:
     exception = context.get("exception")
@@ -49,9 +53,12 @@ class PvlsBotCore:
         self._db = Database(pathlib.Path(os.environ.get("DATABASE_PATH", "/data/discord-bot.sqlite3")))
         self._repo = Repository(self._db)
 
+        self._sync_autofill_choices: list[str] = []
+
         # These resources require allocation from an async context so we can't initialize them here
         self._github: GitHubClient = None
         self._workflow_poll_task: asyncio.Task[None] | None = None
+        self._sync_autocomplete_poll_task: asyncio.Task[None] | None = None
 
     async def setup_hook(self):
         # I hate how asyncio fails silently. Asyncio is the scourge of modern computing but unironically a good option
@@ -66,10 +73,12 @@ class PvlsBotCore:
         _logger.info(f"GitHub client initialized - main hash: {repo_hash}")
 
         await self._db.start()
+        await self._poll_sync_autofill_once();
+        _logger.info("Autofill populated.")
 
         self._workflow_poll_task = asyncio.create_task(self._poll_workflows(), name="workflow-poller")
+        self._sync_autofill_poll_task = asyncio.create_task(self._poll_sync_autofill(), name="sync-autofill-poller")
         _logger.info("Polling task started!")
-
 
     def _register_events(self):
         @self._client.event
@@ -91,8 +100,9 @@ class PvlsBotCore:
             await self._do_sekai(interaction)
 
         @group.command(name="sync", description="Sync and deploy website with updated Google Drive contents")
-        async def sync(interaction: discord.Interaction):
-            await self._do_sync(interaction)
+        @discord.app_commands.autocomplete(song=self._do_sync_autocomplete)
+        async def sync(interaction: discord.Interaction, song: str | None = None):
+            await self._do_sync(interaction, song)
 
         self._tree.add_command(group)
 
@@ -105,16 +115,32 @@ class PvlsBotCore:
         await interaction.response.send_message(line)
 
 
-    async def _do_sync(self, interaction: discord.Interaction):
+    async def _do_sync(self, interaction: discord.Interaction, song_slug: str | None):
         user = interaction.user
         _logger.info(f"User {user.display_name} ({user.id}) started a site sync and deploy")
-        workflow = await self._github.post_workflow("content-sync-and-deploy.yml")
+
+        inputs = {}
+        if song_slug:
+            inputs["song_slug"] = song_slug
+            _logger.info(f"Targeted sync for '{song_slug}' selected.")
+
+        workflow = await self._github.post_workflow("content-sync-and-deploy.yml", inputs=inputs)
         await self._repo.add_workflow(workflow, interaction)
 
         await interaction.response.send_message(
             f"{user.mention} started a site content sync.\n"
             f"GitHub Link: <{workflow.html_url}>"
         )
+
+
+    async def _do_sync_autocomplete(
+        self, interaction: discord.Interaction, current_str: str
+    ) -> list[discord.app_commands.Choice[str]]:
+        return [
+            discord.app_commands.Choice(name=song, value=song)
+            for song in self._sync_autofill_choices if current_str.casefold() in song.casefold()
+        ][:DISCORD_AUTOFILL_COUNT_MAX]
+
 
     async def send_sync_response_message(self, workflow: GitHubWorkflow):
         channel = await self._client.fetch_channel(workflow.channel_id)
@@ -190,6 +216,18 @@ class PvlsBotCore:
             except Exception as e:
                 _logger.error(f"Poll failed - {e}")
 
+    async def _poll_sync_autofill_once(self):
+        generated_manifest = await self._github.download_json_at("frontend/src/data/generated-manifest.json")
+        self._sync_autofill_choices = [pathlib.Path(song).stem for song in generated_manifest["songs"]]
+
+    async def _poll_sync_autofill(self):
+        while True:
+            _logger.debug("Poll all workflows!")
+            try:
+                await self._poll_sync_autofill_once()
+                await asyncio.sleep(3)
+            except Exception as e:
+                _logger.error(f"Poll failed - {e}")
 
     def run(self, token: str):
         self._client.run(token)
