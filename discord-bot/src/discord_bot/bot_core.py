35d5f739b6655b3f42fa1d3239717b0be0a86b5c
Matthew Zhong <mzhong99@vt.edu>
2026-08-20 10:25:43 -0400

    Remove bot scope limits

--- discord-bot/src/discord_bot/bot_core.py
+++ discord-bot/src/discord_bot/bot_core.py
@@ -14,7 +14,6 @@ _logger = logging.getLogger(__name__)
 
 SCRIPT_DIR = pathlib.Path(__file__).parent
 SEKAI_TEXT = (SCRIPT_DIR / "sekai.txt").read_text(encoding="utf-8").splitlines()
-BOT_CHANNEL_NAME = "bot-terminal"
 
 # For some crazy reason, Discord caps the autofill count to 25 and will just reject the command if you try to supply
 # anything more than that
@@ -25,23 +24,6 @@ def asyncio_exception_handler(loop: asyncio.AbstractEventLoop, context: dict[str
     exception = context.get("exception")
     _logger.critical("Unhandled asyncio error: %s", context.get("message", "unknown asyncio error"), exc_info=exception)
 
-
-def find_bot_channel(interaction: discord.Interaction) -> discord.TextChannel | None:
-    if interaction.guild is None:
-        return None
-    return discord.utils.get(interaction.guild.text_channels, name=BOT_CHANNEL_NAME)
-
-
-class WrongChannel(discord.app_commands.CheckFailure):
-    pass
-
-
-class PVLSGroup(discord.app_commands.Group):
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        channel_ok = interaction.channel is not None and interaction.channel.id == find_bot_channel(interaction).id
-        if not channel_ok:
-            raise WrongChannel()
-        return channel_ok
 
 class PvlsBotCore:
     def __init__(self):
@@ -85,15 +67,8 @@ class PvlsBotCore:
         async def on_ready():
             await self.setup_hook()
 
-        @self._tree.error
-        async def on_error(interaction, error):
-            if isinstance(error, WrongChannel):
-                bot_channel = find_bot_channel(interaction)
-                await interaction.response.send_message(f"Please run bot commands in {bot_channel.mention}.", ephemeral=True)
-            return
-
     def _register_commands(self):
-        group = PVLSGroup(name="pvlsbot", description="PVLS bot commands")
+        group = discord.app_commands.Group(name="pvlsbot", description="PVLS bot commands")
 
         @group.command(name="sekai", description="Print the lyrics to World is Mine")
         async def sekai(interaction: discord.Interaction):
